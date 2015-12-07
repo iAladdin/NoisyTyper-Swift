15a9e37fabdaba7b9e204447439310110c257e62
Aladdin <Aladdin@aladdins-rmbp-2.local>
2015-12-07 16:28:09 +0800

    Initial Commit

---
+++ NoisyTyper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoisyTyper
+//
+//  Created by Aladdin on 15/12/7.
+//  Copyright © 2015年 Aladdin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NoisyTyper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoisyTyper
+//
+//  Created by Aladdin on 15/12/7.
+//  Copyright © 2015年 Aladdin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

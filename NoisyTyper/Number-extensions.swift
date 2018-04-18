394f17af08be1028a5f8e48c6d69d207fdd97fcb
Sid_Zheng <zhengt.cn@gmail.com>
2018-04-18 11:04:17 +0800

    Migrate to Swift 4.0

--- NoisyTyper/Number-extensions.swift
+++ NoisyTyper/Number-extensions.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 public extension Float {
-    public static func random(lower: Float = 0, _ upper: Float = 100) -> Float {
+    public static func random(_ lower: Float = 0, _ upper: Float = 100) -> Float {
         return (Float(arc4random()) / 0xFFFFFFFF) * (upper - lower) + lower
     }
 }
 public extension Int {
-    public static func random(lower: Int = 0, _ upper: Int = 100) -> Int {
+    public static func random(_ lower: Int = 0, _ upper: Int = 100) -> Int {
         return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
     }
 }
